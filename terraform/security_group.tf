3e4a20f830af947a319c53cd348413f239be5a01
wim-web <rootuser.main+github@gmail.com>
2021-01-24 21:10:53 +0900

    Add web server

---
+++ terraform/security_group.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "web" {
+  name   = "load-test-web"
+  vpc_id = aws_vpc.this.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "load-test-web"
+  }
+}
---
+++ terraform/web_server.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "web" {
+  count                       = 1
+  instance_type               = "t2.micro"
+  ami                         = data.aws_ssm_parameter.amzn2.value
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public[keys(local.az)[count.index % length(local.az)]].id
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  iam_instance_profile        = aws_iam_instance_profile.ec2.id
+  tags = {
+    "Name" = "load-test-web"
+  }
+}
+
+data "aws_ssm_parameter" "amzn2" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+resource "aws_iam_instance_profile" "ec2" {
+  name = "ec2"
+  role = aws_iam_role.ec2_for_ssm.name
+}
+
+resource "aws_iam_role" "ec2_for_ssm" {
+  name               = "EC2ForSSM"
+  assume_role_policy = file("./policy/ec2_assume_role.json")
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_for_ssm" {
+  role       = aws_iam_role.ec2_for_ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
