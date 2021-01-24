09bf13f2225cbbab1347fb07189979ebd704a2ea
wim-web <rootuser.main+github@gmail.com>
2021-01-25 04:00:46 +0900

    migration

--- laravel/database/migrations/2021_01_24_185127_create_articles_table.php
+++ laravel/database/migrations/2021_01_24_185127_create_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('password');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->text('body');
+            $table->unsignedInteger('like_count')->default(0);
             $table->timestamps();
+            
+            $table->foreign('user_id')->on('users')->references('id');
         });
     }
 
@@ -28,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('articles');
     }
 }
